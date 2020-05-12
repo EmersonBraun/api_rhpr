8935624ec1a7edb7cf1c3f31af2eb4b046a5c6a9
EmersonBraun <emersonfbraun@gmail.com>
2020-05-12 07:53:49 -0300

    fix: solve models namespace

--- app/Models/v0/Ausencia.php
+++ app/Models/v0/Ausencia.php
@@ -1,18 +1,11 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\v0;
 
 use Illuminate\Database\Eloquent\Model;
-// use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Ausencia extends Model
 {
-    /**
-     * When models are soft deleted, they are not actually removed from your database. 
-     * Instead, a deleted_at attribute is set on the model and inserted into the database. 
-     * If a model has a non-null deleted_at value, the model has been soft deleted
-     */
-    // use SoftDeletes;
-
     /**
      * The table associated with the model.
      *
--- app/Models/v0/FuncPriv.php
+++ app/Models/v0/FuncPriv.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\v0;
 
 use Illuminate\Database\Eloquent\Model;
 
